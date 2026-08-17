f1d8dd7e94bc46e61a1d1b10cdfb7202742d4f36
appIeonlineport <indiatryme@gmail.com>
2026-08-17 10:15:40 +0530

    Rebuild config.js for secure responsive NX Leads Hub

---
+++ config.js
@@ -0,0 +1,7 @@
+window.NX_CONFIG = Object.freeze({
+  supabaseUrl: "https://cflbppmzeauepzsydusf.supabase.co",
+  supabaseKey: "sb_publishable_r1tueBq4I7vmJhCJ-xiLpg_gJPmn1sI",
+  walletAddress: "TWhUtsbWiR3gQE6yi9CirRQsR1zKAR9FJd",
+  supportEmail: "support@nxleadshub.com",
+  paymentNetwork: "TRC20"
+});
